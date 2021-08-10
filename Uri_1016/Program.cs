2867c4c6a8732c6c0f30682d12549e8b6d00c84f
RaulORios <raul.orios89@outlook.com>
2021-08-10 11:45:21 -0300

    Arquivos atualizados

---
+++ Uri_1016/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Uri_1016
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int km = int.Parse(Console.ReadLine());
+
+            int tempo_minutos = km * 2;
+
+            Console.WriteLine($"{tempo_minutos} minutos");
+
+        }
+    }
+}
